761d65227e1a4d9833c2720fcee729cde474ffa8
hjm5020 <ghdwhdals1@gmail.com>
2026-04-04 11:52:33 +0900

    docs: 알고리즘 공부 추가

--- docs/js/notesData.js
+++ docs/js/notesData.js
@@ -14,6 +14,15 @@ const NOTES_DATA = [
     notes: [
       { title: 'DB 인덱스 완전 정복', file: 'notes/database/db-index-study.html' }
     ]
+  },
+  {
+    category: 'cs',
+    icon: '💻',
+    name: 'CS / 알고리즘',
+    notes: [
+      { title: '코딩 테스트 핵심 알고리즘', file: 'notes/cs/coding-test-algorithms.html' },
+      { title: 'Java 코딩 테스트 기초 세팅', file: 'notes/cs/java-coding-test-setup.html' }
+    ]
   }
   // 카테고리/노트 추가 시 여기에 추가
   // {
